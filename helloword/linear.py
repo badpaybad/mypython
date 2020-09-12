9d18c94cffae8d66f4c8caa25e8e727f7275cf2f
badpaybad <badpaybad@gmail.com>
2020-09-12 10:55:16 +0700

    xx

--- helloword/linear.py
+++ helloword/linear.py
@@ -1,8 +1,8 @@
 import matplotlib.pyplot as plt
 from scipy import stats
 
-x = [5,7,8,7,2,17,2,9,4,11,12,9,6]
-y = [99,86,87,88,111,86,103,87,94,78,77,85,86]
+x = [1,2,3,4,5,6,7,8,9]
+y = [211,586,539,270,217,413,546,351,338]
 
 slope, intercept, r, p, std_err = stats.linregress(x, y)
 
